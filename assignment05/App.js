ac2ef17b51c6336bed6d43655701afc0d65ecfd8
Apple <apple@Apples-MacBook-Pro.local>
2019-10-17 11:44:16 +0530

    hello

---
+++ assignment05/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+}from "react-native";
+
+
+export default class Form extends Component{
+      
+}
