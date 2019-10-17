08bbc6f8ef0e1341ad50a1cdcee1948d5e5f3ccc
Apple <apple@Apples-MacBook-Pro.local>
2019-10-17 12:30:45 +0530

    added super view

--- assignment05/App.js
+++ assignment05/App.js
@@ -15,88 +15,47 @@ export default class APP extends Component {
     this.state = {
       name: "",
       Email: "",
-      Phone: "",
       Company: "",
-      Designation: "",
-      Profile: "",
-      Address: "",
-      showDetails: false
     };
   }
 
-  onPressSubmit = () => {
-    this.setState({ showDetails: !this.state.showDetails })
-  }
 
-  renderDetails = () => {
-    if (this.state.showDetails) {
-      return (
 
-        <View style={styles.mainView}>
-          <View style={styles.detailShow}>
-            <Text style={styles.text}>Name</Text>
-            <Text style={styles.text}>{this.state.name}</Text>
-          </View>
-          <View style={styles.detailShow}>
-            <Text style={styles.text}>Email</Text>
-            <Text style={styles.text}>{this.state.Email}</Text>
-          </View>
-          
-          <View style={styles.detailShow}>
-            <Text style={styles.text}>Company</Text>
-            <Text style={styles.text}>{this.state.name}</Text>
-          </View>
-
-        </View>
-      );
-    }
-  }
   
   render() {
     return (
-      <ScrollView style={{ flex: 1 }}>
+      <View style={{ flex: 1 ,backgroundColor: "#1799bf",}}> 
+      <Text>User Detail's</Text>
+      <ScrollView >
         <View style={styles.mainView}>
           <View>
             <TextInput placeholder={"Name"} style={styles.textInputStyle}
               onChangeText={(text) => this.setState({ name: text })}
-              value = {this.state.n}
+              
 
             ></TextInput>
           </View>
 
           <View>
             <TextInput placeholder={"Email"} style={styles.textInputStyle} 
-            value = {this.state.Email}
+            
             onChangeText={(text) => this.setState({ Email: text })}></TextInput>
           </View>
-          <View>
-            <TextInput placeholder={"Phone"} style={styles.textInputStyle} 
-            value = {this.state.Phone}
-            onChangeText={(text) => this.setState({ Phone: text })}></TextInput>
-          </View>
+          
           <View>
             <TextInput placeholder={"Company"} style={styles.textInputStyle} 
-            value = {this.state.Company}
+            
             onChangeText={(text) => this.setState({ Company: text })}></TextInput>
           </View>
-          <View>
-            <TextInput placeholder={"Designation"} style={styles.textInputStyle} 
-            value = {this.state.Designation}
-            onChangeText={(text) => this.setState({ Designation: text })}  ></TextInput>
-          </View>
-          <View>
-            <TextInput placeholder={"Profile"} style={styles.textInputStyle}></TextInput>
-          </View>
-          <View>
-            <TextInput placeholder={"Address"} style={styles.textInputStyle}></TextInput>
-          </View>
+          
 
           <TouchableOpacity style={styles.button} onPress={this.onPressSubmit} >
             <Text style={styles.submitButton}>Submit</Text>
           </TouchableOpacity>
-          {this.renderDetails()}
+
         </View>
       </ScrollView>
+      </View>
     );
   }
 }
@@ -107,7 +66,6 @@ const styles = StyleSheet.create(
     mainView: {
       flex: 1,
       justifyContent: "space-around",
-      backgroundColor: "#1799bf",
       paddingLeft: 20,
       paddingRight: 20,
       paddingTop: 50,
